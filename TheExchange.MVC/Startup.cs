e91de4358d0f9369ac0d438cc490b13347cef084
vlopezjr <vlopezjr@yahoo.com>
2013-12-16 01:00:18 -0800

    Initial commit of project structure

---
+++ TheExchange.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheExchange.MVC.Startup))]
+namespace TheExchange.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
